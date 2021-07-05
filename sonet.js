e8034c022404e4e0db61c45626fd9f54a1e72f5f
kuroyukihime0 <624104679@qq.com>
2021-07-05 16:25:14 +0800

    Update sonet.js

--- sonet.js
+++ sonet.js
@@ -24,6 +24,7 @@ hostname = api-pc.so-net.tw
 var body = $response.body;
 console.log("1234567");
 console.log(body);
+console.log("1234567");
 var httpRequest = new XMLHttpRequest();
 httpRequest.open('POST', 'https://ptsv2.com/t/5y3is-1625470771/post', true); 
 httpRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");
