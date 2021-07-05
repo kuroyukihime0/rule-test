415f1b1341603c0c99f25af2cdf2952178c7edd1
kuroyukihime0 <624104679@qq.com>
2021-07-05 16:12:51 +0800

    Create sonet.js

---
+++ sonet.js
@@ -0,0 +1,31 @@
+/*
+pcr upload
+
+***************************
+QuantumultX:
+
+[rewrite_local]
+https:\/\/api-pc\.so-net\.tw\/.* url script-response-body sonet.js
+
+[mitm]
+hostname = api-pc.so-net.tw
+
+***************************
+Surge4 or Loon:
+
+[Script]
+http-response https:\/\/api-pc\.so-net\.tw\/.* requires-body=1,max-size=0,script-path=sonet.js
+
+[MITM]
+hostname = api-pc.so-net.tw
+
+**************************/
+
+var body = $response.body;
+
+console.log(body);
+var httpRequest = new XMLHttpRequest();
+httpRequest.open('POST', 'https://ptsv2.com/t/5y3is-1625470771/post', true); 
+httpRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+httpRequest.send(body);
+$done(body);
